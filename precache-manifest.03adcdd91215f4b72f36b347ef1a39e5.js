3c0acc1d89626ded28782021196e9eef0e8c5b69
Ledezmag <israeledezma@gmail.com>
2020-05-29 00:29:49 -0500

    Updates

---
+++ precache-manifest.03adcdd91215f4b72f36b347ef1a39e5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c5eda0df9caee1debbc45736f62793",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46055030914ec2cbcb61",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "c1d2e6761e620fa8be5d",
+    "url": "/static/css/main.eb2cbef0.chunk.css"
+  },
+  {
+    "revision": "46055030914ec2cbcb61",
+    "url": "/static/js/2.2a5d9120.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2a5d9120.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1d2e6761e620fa8be5d",
+    "url": "/static/js/main.163680d6.chunk.js"
+  },
+  {
+    "revision": "95e085083857c8534e0b",
+    "url": "/static/js/runtime-main.483924be.js"
+  },
+  {
+    "revision": "0994c8880dcaf481bfc746ba013f1279",
+    "url": "/static/media/T-rex.0994c888.png"
+  }
+]);
